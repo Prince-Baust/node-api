f9b325215b41dc8b7b89162684c5c2e7144bb26d
Md. Maruf Al Hossain Prince <pmaruf42@gmail.com>
2019-11-02 21:06:39 +0600

    database create

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+
+mongoose.connect("mongodb://localhost:27017/node_api_DB", {useUnifiedTopology: true, useNewUrlParser: true}, function () {
+    console.log('database started!');
+});
 
 app.get('/', (req, res, next) =>{
     res.send('running node api');
 });
-
 
 app.listen(3000, function () {
    console.log("Server started on port 3000");
